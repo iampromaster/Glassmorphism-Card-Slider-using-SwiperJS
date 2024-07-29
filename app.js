cd6198cd24d79e2c6a589f868f981a46df4da06a
pypymi <dr.mizoguchi@gmail.com>
2024-07-29 12:55:16 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
